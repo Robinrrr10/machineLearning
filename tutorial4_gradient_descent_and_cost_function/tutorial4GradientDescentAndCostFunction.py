57531fe55984515cfb1189372bb1d652af116e25
Robin R <44073590+Robinrrr10@users.noreply.github.com>
2025-08-24 14:06:41 +0530

    modified cost equation in tutorial4

--- tutorial4_gradient_descent_and_cost_function/tutorial4GradientDescentAndCostFunction.py
+++ tutorial4_gradient_descent_and_cost_function/tutorial4GradientDescentAndCostFunction.py
@@ -7,7 +7,7 @@ def gradient_descent(x, y):
     learning_rate = 0.08  # we can take any decimal number to slowly. Eg: 0.01 or 0.001 or 0.005 and then we can fine tune using trail and error
     for i in range(iterations):
         y_predict = m_curr * x + b_curr   # giving formula y = mx+b
-        cost = (1/2) * sum([val**2 for val in (y - y_predict)]) # this formula for finding cost
+        cost = (1/n) * sum([val**2 for val in (y - y_predict)]) # this formula for finding cost
         md = -(2/n)*sum(x*(y - y_predict)) # here also giving other forumla
         bd = -(2/n)*sum(y - y_predict) # here also given other formula
         m_curr = m_curr - learning_rate * md
@@ -21,5 +21,6 @@ def gradient_descent(x, y):
 
 x = np.array([1, 2, 3, 4, 5])    #usually numpy array is faster
 y = np.array([5, 7, 9, 11, 13])
+
 
 gradient_descent(x,y)
